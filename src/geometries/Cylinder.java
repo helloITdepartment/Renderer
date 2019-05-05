db67de67e71c3f78143be6717060d83ecd4ad4b8
Jacques Benzakein <Jackbenzakein@gmail.com>
2019-05-05 14:34:56 +0300

    Updated comments for the Cylinder and Plane classes

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -5,46 +5,55 @@ import java.util.List;
 import primitives.*;
 
 public class Cylinder extends RadialGeometry implements Geometry{
+	//Ray representing the central axis around which the cylinder revolves, at a radius inherited from RadialGeometry
 	Ray _axisRay;
-	
+
 	//Constructors
 	//Empty constructor
 	public Cylinder(){
+		//use super class to create basic geometric forms
 		super();
+		//Implement the axis ray using a Ray constructor
 		_axisRay = new Ray();
 	}
-			
+
 	//Parameterized constructors
 	public Cylinder(Ray axisRay){
+		//use super class to create basic geometric form
 		super();
+		//set the axisRay with the passed axisRay
 		_axisRay = axisRay;
 	}
-	
+
 	public Cylinder(double radius, Ray axisRay){
+		//constructor given the radius and axisRay as parameters
 		_radius = radius;
 		_axisRay = axisRay;
 	}
-		
+
 	//Copy constructor
 	public Cylinder(Cylinder other){
+		//copies the data members from the passed cylinder to the cylinder it was called on
 		_radius = other._radius;
 		_axisRay = other._axisRay;
 	}
-		
+
 	//Getters
 	//radius getter is unnecessary as it's inherited from RadialGeometry
 	public Ray getAxisRay(){
+		//Returns new Ray with the same value as our _axisRay, so that changes made at the callsite wont affect our variables
 		return new Ray(_axisRay);
 	}
-	
+
 	//Setters
 	//radius setter is unnecessary as it's inherited from RadialGeometry
 	public void setAxisRay(Ray axisRay){
+		//allows us to set a value for the axisRay data member
 		_axisRay = axisRay;
 	}
 
 	public List<Point3D> findIntersection(Ray r) {
-		// TODO Auto-generated method stub
+		//Returns a (possibly empty) list of points where a given Ray r would intersect our Cylinder
 		List<Point3D> listToReturn = new ArrayList<>();
 		return listToReturn;
 	}
--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -5,77 +5,95 @@ import java.util.ArrayList;
 import primitives.*;
 
 public class Plane implements Geometry{
+	//Any plane can be uniquely represented by 3 points
 	Point3D _p1;
 	Point3D _p2;
 	Point3D _p3;
+	//The vector normal to the plane
 	Vector _normal;
-	
+
 	//Constructors
 	//Empty constructor
 	public Plane(){
+		//Creates a plane with three default points and a default normal to the plane
 		_p1 = new Point3D();
 		_p2 = new Point3D();
 		_p3 = new Point3D();
 		_normal = new Vector();
 	}
-			
+
 	//Parameterized constructor
 	public Plane(Point3D p1, Point3D p2, Point3D p3){
+		//creates a plane with three points passed in as parameters and calculates the normal vector
 		_p1 = p1;
 		_p2 = p2;
 		_p3 = p3;
 		_normal = new Vector(p2.subtract(p1)).crossProduct(new Vector(p3.subtract(p1))).normalize();
 	}
-		
+
 	//Copy constructor
 	public Plane(Plane other){
+		//copies the points from the passed plane to the plane it is called on
 		_p1 = other._p1;
 		_p2 = other._p2;
 		_p3 = other._p3;
 	}
-		
+
 	//Getters
 	public Point3D getP1(){
+		//Returns new Point3D with the same value as our _p1, so that changes made at the callsite wont affect our variables
 		return new Point3D(_p1);
 	}
-	
+
 	public Point3D getP2(){
+		//Returns new Point3D with the same value as our _p2, so that changes made at the callsite wont affect our variables
 		return new Point3D(_p2);
 	}
-	
+
 	public Point3D getP3(){
+		//Returns new Point3D with the same value as our _p3, so that changes made at the callsite wont affect our variables
 		return new Point3D(_p3);
 	}
-	
+
 	//Setters
 	public void setP1(Point3D p1){
+		//Allows for the setting of the p1 data member from the passed parameters
 		_p1 = p1;
 	}
-	
+
 	public void setP2(Point3D p2){
+		//Allows for the setting of the p2 data member from the passed parameters
 		_p2 = p2;
 	}
-	
+
 	public void setP3(Point3D p3){
+		//Allows for the setting of the p3 data member from the passed parameters
 		_p3 = p3;
 	}
 
 	public List<Point3D> findIntersection(Ray r) {
-		// TODO Auto-generated method stub
+		//Initializes an empty list that will contain the point(s) of intersection, if any
 		List<Point3D> listToReturn = new ArrayList<>();
-		
+
+		//denominator holds the result of the dot product between the ray and the plane
 		double denominator = _normal.dotProduct(r.getDirection());
-		
+
+		//if the dot product returns zero it means the ray is perpendicular to the normal which is perpendicular to the plane and therefore runs parallel to the plane and does not intersect
 		if(denominator == 0) {
+			//returns an empty list of intersection
 			return listToReturn;
 		}
-		
+
+		//t holds the distance from the source of the ray (camera) to the plane
 		double t = (new Vector(_p1.subtract(r.getSource())).dotProduct(_normal))/denominator;
-		
+
+		//We scale the Ray by t to get the point of intersection of the ray and the plane
 		Point3D intersectedPoint = new Point3D(r.getSource().add(r.getDirection().scale(t)));
-		
+
+		//we add the new point of intersection to our list
 		listToReturn.add(intersectedPoint);
-		
+
+		//return the list of intersections
 		return listToReturn;
 	}
 }
